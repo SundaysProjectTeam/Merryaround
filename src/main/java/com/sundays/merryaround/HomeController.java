8a55146843d6f02dc95e73adcd23c8b329775edd
yjkim <yjkim@digicaps.com>
2019-11-14 18:24:48 +0900

    first commit

--- src/main/java/com/sundays/merryaround/HomeController.java
+++ src/main/java/com/sundays/merryaround/HomeController.java
@@ -1,5 +1,23 @@
 package com.sundays.merryaround;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/main")
+	public String indexPage() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String loginPage() {
+		return "member/login";
+	}
+	
+	@GetMapping("/join")
+	public String joinPage() {
+		return "member/join";
+	}
 }
